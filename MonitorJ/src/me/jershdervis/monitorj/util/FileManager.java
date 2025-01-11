f6909ed8be4ff52b6fbce515887a87aebb2709a8
XeContrast <1683481541@qq.com>
2025-01-11 21:06:37 +0800

    我操你妈的逼

--- MonitorJ/src/me/jershdervis/monitorj/util/FileManager.java
+++ MonitorJ/src/me/jershdervis/monitorj/util/FileManager.java
@@ -1,50 +1,48 @@
 package me.jershdervis.monitorj.util;
 
-import com.alibaba.fastjson2.JSONArray;
-import com.alibaba.fastjson2.JSONObject;
-import com.alibaba.fastjson2.JSONWriter;
 import me.jershdervis.monitorj.eventapi.EventManager;
 import me.jershdervis.monitorj.eventapi.EventTarget;
 import me.jershdervis.monitorj.eventapi.events.EventUILoaded;
+import me.jershdervis.monitorj.server.ServerManager;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
+import javax.swing.table.DefaultTableModel;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Josh on 20/07/2015.
  */
 public class FileManager {
 
-    private final File settingsFile = new File("settings.json");
-    private final JSONObject socketsDocument;
+    private final File settingsFile = new File("settings.xml");
+    private Document socketsDocument;
 
     public FileManager() {
         EventManager.register(this);
-
-        JSONArray socketsElement = new JSONArray("sockets");
-        JSONObject jobj = new JSONObject();
-        this.socketsDocument = socketsElement.addObject();
+        Element socketsElement = new Element("sockets");
+        this.socketsDocument = new Document(socketsElement);
     }
 
     /**
      * Adds the formatted socket value to the XML file
-     *
      * @param name
      * @param port
      * @param desc
      */
-    public void saveSocketValue(String name, int port, String desc) throws IOException {
-//        Element newSocket = new Element("socket");
-        JSONObject jsonObject = new JSONObject();
-//        newSocket.addContent(new Element("name").setText(name));
-        jsonObject.put("name", name);
-//        newSocket.addContent(new Element("port").setText(String.valueOf(port)));
-        jsonObject.put("port", port);
-//        newSocket.addContent(new Element("desc").setText(desc));
-        jsonObject.put("desc", desc);
-//        this.socketsDocument.getRootElement().addContent(newSocket);
-
+    public void saveSocketValue(String name, int port, String desc) {
+        Element newSocket = new Element("socket");
+        newSocket.addContent(new Element("name").setText(name));
+        newSocket.addContent(new Element("port").setText(String.valueOf(port)));
+        newSocket.addContent(new Element("desc").setText(desc));
+        this.socketsDocument.getRootElement().addContent(newSocket);
         this.saveXmlFile(socketsDocument, settingsFile);
     }
 
@@ -54,26 +52,71 @@ public class FileManager {
      * @param port
      */
     public void removeSocketValue(int port) {
-        System.out.println("你妈了个逼removeSocketValue是炸的");
+        SAXBuilder saxBuilder = new SAXBuilder();
+        try {
+            // converted file to document object
+            Document document = saxBuilder.build(this.settingsFile);
+
+            Element rootNode = document.getRootElement();
+
+            List<Element> socketList = rootNode.getChildren("socket");
+
+            for(Element element : socketList) {
+                if(port == Integer.parseInt(element.getChildText("port"))) {
+                    this.socketsDocument.getRootElement().removeContent(element);
+                }
+            }
+
+            this.saveXmlFile(this.socketsDocument, this.settingsFile);
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    void saveXmlFile(JSONObject doc, File xmlFile) throws IOException {
-        FileWriter fileWriter = new FileWriter(xmlFile);
-        JSONWriter jsonWriter = JSONWriter.of();
-        jsonWriter.write(doc);
-        fileWriter.write(jsonWriter.toString());
-        fileWriter.flush();
-        fileWriter.close();
+    private void saveXmlFile(Document doc, File xmlFile) {
+        try {
+            XMLOutputter xmlOutput = new XMLOutputter();
+            xmlOutput.setFormat(Format.getPrettyFormat());
+            xmlOutput.output(doc, new FileWriter(xmlFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Loads Server Socket Listeners when program is initialized
-     *
      * @param event
      */
     @EventTarget
     public void onUILoaded(EventUILoaded event) {
-        System.out.println("你妈了个逼onUILoaded被我搞炸了");
+        if(this.settingsFile.exists()) {
+            SAXBuilder saxBuilder = new SAXBuilder();
+            try {
+                // converted file to document object
+                this.socketsDocument = saxBuilder.build(this.settingsFile);
+
+                Element rootNode = this.socketsDocument.getRootElement();
+
+                List<Element> socketList = rootNode.getChildren("socket");
+
+                for (Element element : socketList) {
+                    String name = element.getChildText("name");
+                    int port = Integer.parseInt(element.getChildText("port"));
+                    String desc = element.getChildText("desc");
+
+                    ServerManager.instance.listenOnPort(port);
+
+                    DefaultTableModel model = (DefaultTableModel) event.getUi().socketTable.getModel();
+                    model.addRow(new Object[]{name, String.valueOf(port), desc});
+                }
+            } catch (JDOMException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
